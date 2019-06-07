704c2889695ded59cb8bc7618baffe22a733c47a
Meg Galvez <meg@oakmaine.com>
2019-06-07 21:30:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each {|key, value| self.send()
+    student_hash.each {|key, value| self.send(("#{key}"))
     @name = student_hash[:name]
     @location = student_hash[:location]
     @twitter = student_hash[:twitter]
