3cb3b261fbbc455b4845016779eaadcea648fad3
Meg Galvez <meg@oakmaine.com>
2019-06-07 21:27:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    new_student = Student.new 
+    new_student.name = students_array
   end
 
   def add_student_attributes(attributes_hash)
